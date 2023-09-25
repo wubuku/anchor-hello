6864960113cbed35b47681e162fc259bdd8f9a19
Yang Jiefeng <yangjiefeng@gmail.com>
2023-09-25 13:54:57 +0800

    refactor

--- programs/hello/src/context.rs
+++ programs/hello/src/context.rs
@@ -4,14 +4,14 @@ use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct Initialize<'info> {
     #[account(
-    init,
-    payer = authority,
-    space = 8 + HelloWorld::INIT_SPACE,
-    seeds = [
-    b"hello-world",
-    authority.key().as_ref(),
-    ],
-    bump
+        init,
+        payer = authority,
+        space = 8 + HelloWorld::INIT_SPACE,
+        seeds = [
+            b"hello-world",
+            authority.key().as_ref(),
+        ],
+        bump
     )]
     pub hello_world: Account<'info, HelloWorld>,
     //
